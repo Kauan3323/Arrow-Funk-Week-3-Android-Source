b06830e86287241767aa3d408a10cbe67caec3eb
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-23 12:46:10 -0300

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,5 +1,6 @@
 package;
 
+#if !android
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
 
@@ -84,7 +85,7 @@ class DiscordClient
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
@@ -98,3 +99,4 @@ class DiscordClient
 	}
 	#end
 }
+#end
